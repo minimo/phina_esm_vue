e0e9941c90c6b2c09f5dcc02ca6a408ab7d75489
yoshifumi.fujimoto <yoshifumi.fujimoto@alphacode.co.jp>
2021-01-06 17:00:16 +0900

    build

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ Vue.config.productionTip = false
 DisplayScene.defaults.width = 960;
 DisplayScene.defaults.height = 640;
 
-console.log('Version: 0.1.0 (Date 2020-12-29, Commit #fb786d5)');
+console.log('Version: 0.1.0 (Date 2021-01-06, Commit #9fc57bb)');
 if (process.env.NODE_ENV !== 'production') {
   console.log('##### Development mode #####');
 }
