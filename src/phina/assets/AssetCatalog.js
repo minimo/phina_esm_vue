0ccf9c154703becae1f87380e32effc95d301d48
minimo <minimo.mobile@gmail.com>
2021-01-06 23:35:16 +0900

    add asset

--- src/phina/assets/AssetCatalog.js
+++ src/phina/assets/AssetCatalog.js
@@ -1,6 +1,10 @@
 export const AssetCatalog = {
   image: {
-    "tomapiyo": "./assets/images/tomapiyo.png",
+    "player1": "./assets/images/tomapiyo.png",
+    "player2": "./assets/images/meropiyo.png",
+    "player3": "./assets/images/mikapiyo.png",
+    "player4": "./assets/images/nasupiyo.png",
+    "player5": "./assets/images/takepiyo.png",
     "background": "./assets/images/background.png",
     "tube1": "./assets/images/tube1.png",
     "tube2": "./assets/images/tube2.png",
