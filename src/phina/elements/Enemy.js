cc52557a7ebf560e3f3066d6f1b0d9dc1aa70791
minimo <minimo.mobile@gmail.com>
2021-01-30 01:33:24 +0900

    update

---
+++ src/phina/elements/Enemy.js
@@ -0,0 +1,16 @@
+import { CircleShape, RectangleShape } from "phina.js/build/phina.esm";
+import { $safe } from "../extensions/Utils";
+import { GameObject } from "./GameObject";
+
+export class Enemy extends GameObject {
+  constructor(options) {
+    options = $safe.call({}, options, { width: 16, height: 16 });
+    super(options);
+
+    // this.sprite = new Sprite("", 64, 64).addChildTo(this);
+    this.sprite = new CircleShape({ fill: 'red', stroke: '#aaa', strokeWidth: 1, radius: 16 }).addChildTo(this);
+
+    this.collision = new RectangleShape({ width: 16, height: 16 }).addChildTo(this);
+    this.collision.alpha = 0.0;
+  }
+}
